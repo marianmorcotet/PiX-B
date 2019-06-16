7eb3f4970010cb8e479ba76349b1d6f9f8d645f4
cojocarupaul <cojocarupaul31@gmail.com>
2019-06-16 20:35:55 +0300

    Delete picture with tags.

--- delete.php
+++ delete.php
@@ -1,7 +1,14 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "", "test"); 
+$connect = mysqli_connect("localhost", "root", "", "pixData"); 
 $id=$_GET["id"];
-mysqli_query($connect,"delete from tbl_images where id=$id");
+$result=mysqli_query($connect,"select id_tag from tagrelations where id_picture=$id LIMIT 1");
+while($row = mysqli_fetch_array($result)){
+    $idTag=$row["id_tag"];
+    mysqli_query($connect,"delete from tagrelations where id_picture=$id and id_tag=$idTag");
+    mysqli_query($connect,"delete from tags where id_tag=$idTag");
+    $result=mysqli_query($connect,"select id_tag from tagrelations where id_picture=$id LIMIT 1");
+}
+mysqli_query($connect,"delete from Pictures where id_picture=$id");
 mysqli_close($connect);
 ?>
 
