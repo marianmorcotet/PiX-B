1ac50c9367752b47d009aef411516c8c3b29c1d0
marianmorcotet <marian.m177@yahoo.com>
2019-06-17 11:33:30 +0300

    download problems
    download now saves the correct resolution

--- scripts/edit.js
+++ scripts/edit.js
@@ -3,6 +3,11 @@ var image = document.getElementById("editedImage");
 var context = canvas.getContext('2d');
 var filterControls = document.querySelectorAll("input[type=range]");
 
+
+var newCanvas = document.createElement("canvas");
+var imageWidth = 0;
+var imageHeight = 0;
+
 function scaleToFit(img){
     // get the scale
     var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
@@ -10,6 +15,16 @@ function scaleToFit(img){
     var x = (canvas.width / 2) - (img.width / 2) * scale;
     var y = (canvas.height / 2) - (img.height / 2) * scale;
     context.drawImage(img, x, y, img.width * scale, img.height * scale);
+
+    imageHeight = (img.height * scale) - y;
+    imageWidth = (img.width * scale) - x;
+
+    newCanvas.width = imageWidth;
+    newCanvas.height = imageHeight;
+
+    var newContext = newCanvas.getContext('2d');
+
+    newContext.drawImage(canvas, x, y, img.width * scale, img.height * scale, 0, 0, newCanvas.width, newCanvas.height);
 }
 
 function applyFilter(){
@@ -22,15 +37,17 @@ function applyFilter(){
 
     scaleToFit(image);
 
-    hiddenInput.value = canvas.toDataURL("image/png");
+    hiddenInput.value = newCanvas.toDataURL("image/png");
 };
 
 function addDownloadLink(){
     var link = document.createElement('a');
+    
     link.innerHTML = 'download image';
+
     link.addEventListener('click', function(ev) {
-    link.href = canvas.toDataURL();
-    link.download = "mypainting.png";
+        link.href = newCanvas.toDataURL();
+        link.download = "mypainting.png";
 }, false);
 var bottomMenu = document.getElementById("bottomMenu");
 bottomMenu.appendChild(link);
