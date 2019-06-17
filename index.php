8f090e37d9cd58b1c9f4e1c7103489eecd0a2376
cojocarupaul <cojocarupaul31@gmail.com>
2019-06-17 11:46:20 +0300

    Test

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['userId'])){
-    header("Location: Gallery.php");
-}
-
-include("login.html");
-include("sharedFunctions.php");
- ?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("sharedFunctions.php");
-session_start();
-deletePersistentSession(session_id());
-session_destroy();
-header('Location: index.php');
-exit;
-?>
