0e8bdc5b7f30f249ad24e8e57a2ca18f48fcb917
marianmorcotet <marian.m177@yahoo.com>
2019-06-17 12:15:30 +0300

    delete?

--- delete.php
+++ delete.php
@@ -1,7 +1,6 @@
 <?php
 $connect = mysqli_connect("localhost", "root", "", "pixData"); 
 $id=$_GET["id"];
-<<<<<<< HEAD
 $result=mysqli_query($connect,"select id_tag from tagrelations where id_picture=$id LIMIT 1");
 while($row = mysqli_fetch_array($result)){
     $idTag=$row["id_tag"];
@@ -9,8 +8,6 @@ while($row = mysqli_fetch_array($result)){
     mysqli_query($connect,"delete from tags where id_tag=$idTag");
     $result=mysqli_query($connect,"select id_tag from tagrelations where id_picture=$id LIMIT 1");
 }
-=======
->>>>>>> master
 mysqli_query($connect,"delete from Pictures where id_picture=$id");
 mysqli_close($connect);
 ?>
